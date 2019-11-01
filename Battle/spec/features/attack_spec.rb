a3f1070c0759c1989579ad2db53d625e93df0cf4
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-01 14:10:50 +0000

    attack

---
+++ Battle/spec/features/attack_spec.rb
@@ -0,0 +1,10 @@
+feature "Attack Player" do 
+   scenario "attack Player 2" do 
+     sign_in_and_play
+     click_link "Attack"
+     expect(page).to have_content 'Shubs attacked Kenny'
+   end 
+  end 
+
+
+
